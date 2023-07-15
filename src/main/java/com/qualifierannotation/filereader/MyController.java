51479e2949bf854583d534ba226931eff6b8edb3
okanboke <okanbokee@gmail.com>
2023-07-15 20:04:27 +0300

    initial commit

--- src/main/java/com/qualifierannotation/filereader/MyController.java
+++ src/main/java/com/qualifierannotation/filereader/MyController.java
@@ -10,6 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MyController {
 
+
+    /*
+    IoC Container inerisinde bulunan intecj edilebilecek nesneler
+    pdfFileReader
+    excelFileReader
+    getWordFileReader
+    @Primary anotasyonunu herhangi bir class'a yazarsak -
+    Qualifier anotasyonunu kullanmadan birincil olarak belirleyebiliriz.
+     */
+
     @Autowired
     @Qualifier("PDF") //iki sınıf IoC içerisinde olduğu için Qualifier
     // anotasyonu ile hangi sınıfı çalıştıracağını yazıyoruz.
